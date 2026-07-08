e801edbc7bf630e3a708985103324aa239d3d54a
selvi.kombaiah <selvi.kombaiah@mallow-tech.com>
2026-07-08 11:05:33 +0530

    Dummy commit to check GitHub standards

--- app/src/main/java/com/example/githubstandards/MainActivity.kt
+++ app/src/main/java/com/example/githubstandards/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.githubstandards
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -29,6 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GitHubStandardsTheme {
                 GitHubStandardsApp()
+                Log.d("Test","Test log")
             }
         }
     }
