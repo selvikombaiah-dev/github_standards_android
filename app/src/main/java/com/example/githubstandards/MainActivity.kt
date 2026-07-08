62b5e755ebd055ab9f76a9f1f1b2a0e03389208f
selvi.kombaiah <selvi.kombaiah@mallow-tech.com>
2026-07-08 15:28:59 +0530

    Dummy commit4 to check GitHub Merge & Rebase option

--- app/src/main/java/com/example/githubstandards/MainActivity.kt
+++ app/src/main/java/com/example/githubstandards/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : ComponentActivity() {
                 Log.d("Test","Test log")
                 Log.d("Test","Release log1")
                 Log.d("Test","Release log2")
+                Log.d("Test","Release log3")
             }
         }
     }
